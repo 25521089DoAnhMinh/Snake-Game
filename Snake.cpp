2d94b2d6cc273d9889d22bd29e4ceff134bc23e2
PhanChiThien3001 <25521755@gm.uit.edu.vn>
2026-04-03 17:15:04 +0700

    an-moi

--- Snake.cpp
+++ Snake.cpp
@@ -2,27 +2,43 @@
 #include <windows.h>
 #include <cstdlib>
 #include <conio.h>
+#include <ctime>
+
 using namespace std;
 void gotoxy( int column, int line );
 struct Point{
     int x,y;
 };
+
 class CONRAN{
 public:
     struct Point A[100];
     int DoDai;
+    struct Point Moi;
+
     CONRAN(){
         DoDai = 3;
         A[0].x = 10; A[0].y = 10;
         A[1].x = 11; A[1].y = 10;
         A[2].x = 12; A[2].y = 10;
+        PhatSinhMoi();
     }
+
+    void PhatSinhMoi(){
+        Moi.x = rand() % 20 + 1;
+        Moi.y = rand() % 20 + 1;
+    }
+
     void Ve(){
+        gotoxy(Moi.x, Moi.y);
+        cout << "O";
+
         for (int i = 0; i < DoDai; i++){
             gotoxy(A[i].x,A[i].y);
             cout<<"X";
         }
     }
+
     void DiChuyen(int Huong){
         for (int i = DoDai-1; i>0;i--)
             A[i] = A[i-1];
@@ -30,12 +46,19 @@ public:
         if (Huong==1) A[0].y = A[0].y + 1;
         if (Huong==2) A[0].x = A[0].x - 1;
         if (Huong==3) A[0].y = A[0].y - 1;
+    }
 
+    void XuLyAnMoi(){
+        if (A[0].x == Moi.x && A[0].y == Moi.y){
+            DoDai++;
+            PhatSinhMoi();
+        }
     }
 };
 
 int main()
 {
+    srand(time(0));
     CONRAN r;
     int Huong = 0;
     char t;
@@ -51,12 +74,14 @@ int main()
         system("cls");
         r.Ve();
         r.DiChuyen(Huong);
+
+        r.XuLyAnMoi();
+
         Sleep(300);
     }
 
     return 0;
 }
-
 
 void gotoxy( int column, int line )
   {
